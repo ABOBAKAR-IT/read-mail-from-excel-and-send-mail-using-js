21afef1a4e3a7ed817c41607ad798b1f8a71c009
RANA ABOBAKAR <ranaabobakarit@gmail.com>
2021-11-18 16:31:42 +0500

    read excel file done

--- index.js
+++ index.js
@@ -8,14 +8,15 @@ wb.xlsx.readFile(filePath).then(function(){
 
     var sh = wb.getWorksheet("Sheet1");
 
-    sh.getRow(1).getCell(2).value = 32;
+    sh.getRow(1).getCell(2).value;
     wb.xlsx.writeFile("sample2.xlsx");
-    console.log("Row-3 | Cell-2 - "+sh.getRow(3).getCell(2).value);
+   // console.log("Row-3 | Cell-2 - "+sh.getRow(3).getCell(2).value);
 
     console.log(sh.rowCount);
     //Get all the rows data [1st and 2nd column]
+    //for(let x=0;x<3;x++)
     for (i = 1; i <= sh.rowCount; i++) {
-        console.log(sh.getRow(i).getCell(1).value);
-        console.log(sh.getRow(i).getCell(2).value);
+        console.log(sh.getRow(i).getCell(1).value+" "+sh.getRow(i).getCell(2).value+" "+sh.getRow(i).getCell(3).value+" "+sh.getRow(i).getCell(4).value);
+      
     }
 });
